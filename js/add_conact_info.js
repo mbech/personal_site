4a3d6178aa24df809f787d1841406162261fb2a4
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-21 17:48:37 -0700

    Href in mailto link now sets correctly, handles multiple instances per window

--- js/add_conact_info.js
+++ js/add_conact_info.js
@@ -1,12 +1,16 @@
 var contact_info = {
-   site_url: "mbech.net"
+  site_url: "mbech.net"
 }
 
 contact_info.add_contact = function(name){
   var nodes = document.getElementsByClassName('site-contact');
-  var node = nodes[0];
+  for (var i = 0; i < nodes.length; i ++) {
+    var node = nodes[i];
+    var parentNode = node.parentNode;
+    var email_address = name + "@" + contact_info.site_url;
 
-  node.innerHTML =  name + "@" + contact_info.site_url;
-
+    node.innerHTML = email_address; 
+    parentNode.href = "mailto:" + email_address; 
+  }
 };
 
